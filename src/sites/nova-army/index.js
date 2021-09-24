456d1c3969426faa715c5d02243db47bd356f6c3
M2rsho <mariuszbubak10@gmail.com>
2021-09-24 17:09:56 +0200

    Yes

--- src/sites/nova-army/index.js
+++ src/sites/nova-army/index.js
@@ -1,11 +1,13 @@
 import './index.css';
+import {Helmet} from "react-helmet";
 //import { Twemoji } from 'react-emoji-render';
+
 
 const NovaArmy = () => {
   document.title = "Nova Army - M2rsho.cf"
   return (
     <>
-    <head>
+    <Helmet>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#D6D7D2" />
         <meta name="description" content="We Want You For Nova Simp Army!"/>
@@ -13,8 +15,8 @@ const NovaArmy = () => {
         <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
         <meta property="og:image"  content="%PUBLIC_URL%/UncleSam.jpg" />
         <meta property="twitter:image"  content="%PUBLIC_URL%/UncleSam.jpg" />
-        
-    </head>
+    </Helmet>
+
     <div className="App">
       <header className="App-header">
           <img src="assets/UncleSam.jpg" alt="Uncle Sam" style={{width: "40vw"}}/>
