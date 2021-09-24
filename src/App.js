2f1e96c0337a6ddc1e9c626e7470712a77f884da
M2rsho <mariuszbubak10@gmail.com>
2021-09-24 14:35:09 +0200

    Wack Icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import icon from './icon.svg'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <img src={icon} className="App-logo" alt="logo" />
           <pre>Cumming Soon...</pre>
       </header>
     </div>
