b935a2fc682c56bae6a2c8d7f67034982ad01241
mariyt10 <mariuszbubak10@gmail.com>
2021-12-12 18:59:23 +0100

    :trollface:

--- src/Pages/main/index.js
+++ src/Pages/main/index.js
@@ -5,7 +5,7 @@ class Main extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Coming Soon...</h1>
+        <h1>Cuming Soon...<span style={{fontSize: "0px"}}>Daddy</span></h1>
       </div>
     );
   }
