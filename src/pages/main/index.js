06be5060c49d5bf7c5e7708948ffbdbe2908d4b6
mariyt10 <mariuszbubak10@gmail.com>
2022-01-12 18:52:00 +0100

    Dot

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,7 +8,7 @@ class Main extends React.Component {
         <div className="App">
           <div className="filter" style={{ position: "relative", top: "-100px", padding: "25px", borderRadius: "15px" }}>
             <h1>
-              Coming Soon..<span style={{ fontSize: ".1px" }}> Daddy UwU</span>.
+              Coming Soon.<span style={{ fontSize: ".1px" }}> Daddy UwU</span>..
             </h1>
           </div>
         </div>
